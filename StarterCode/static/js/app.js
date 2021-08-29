d71d2b40294a2f40c457099fc0df78d972c60d34
rls1316 <80739270+rls1316@users.noreply.github.com>
2021-08-29 00:27:35 -0700

    saving point

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,30 @@
+// Set function for dropdown start
+function init () {
+
+    // Use d3 to select the dropdown menu
+    var dropdownMenu = d3.select("#selDataset");
+
+    // Read sample data json with D3
+    d3.json("samples.json").then((samplesdata) => {
+        var samplesname = samplesdata.names;
+        console.log(samplesdata);
+
+        // Assign the value of the dropdown menu option to a variable
+        samplesname.forEach((sampleitem) => {
+            dropdownMenu.append("option")
+            .text(sampleitem)
+            .property("value", sampleitem);
+        })
+
+        // Set default value and build charts
+        var defaultname = samplesname[0];
+
+        Charts(defaultname);
+        MetaData(defaultname);
+    });
+};
+
+// Call function for dropdown start
+init ();
+
+// Set function for Charts
