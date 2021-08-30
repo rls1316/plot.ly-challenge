8d4eea55790fc9845ccfea3b0531198a7ad34efc
rls1316 <80739270+rls1316@users.noreply.github.com>
2021-08-30 15:37:28 -0700

    saving point

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -16,15 +16,75 @@ function init() {
             .property("value", sampleitem);
         })
 
-        // Set default value and build charts
+        // Set default value
         var defaultname = samplesname[0];
 
-        //Charts(defaultname);
-       // MetaData(defaultname);
+        // Chart defaults
+        Charts(defaultname);
+        // MetaData(defaultname);
     });
-}
+};
 
 // Call function for dropdown start
 init();
 
-// Set function for Charts
+// Set function for Charts (bar and bubble)
+function Charts(sampleitem) {
+
+    // Variables to hold array and filter for selected sample
+    d3.json("./data/samples.json").then((samplesdata) => {
+        var dataitems = samplesdata.samples;
+        var arrayresult = dataitems.filter(sampleobj => sampleobj.id == sampleitem);
+
+        // Set default value 
+        var filteredresult = arrayresult[0];
+        console.log(filteredresult);
+
+        // Gather horizontal bar chart and bubble chart
+        var otuID = filteredresult.otu_ids;
+        var otuLabels = filteredresult.otu_labels;
+        var samplevalues = filteredresult.sample_values;
+
+        // Set bar chart 
+        var barchart = [{
+            x: samplevalues.slice(0,10).reverse(),
+            y: otuID.slice(0,10).map((id) => "OTU" + id).reverse(),
+            text: otuLabels.slice(0,10).reverse(),
+            type: "bar",
+            orientation: "h",
+        }];
+
+        var barlayout = {
+            title: 'Top 10 Bacteria Cultures Found',
+            font:{
+                family: 'Raleway, sans-serif',
+            }
+        };
+
+        Plotly.newPlot("bar", barchart, barlayout)
+
+        // Set bubble chart
+        var bubblechart = [{
+            x: otuID,
+            y: samplevalues,
+            text: otuLabels,
+            mode: "markers",
+            marker: {
+                size: samplevalues,
+                color: otuID,
+                colorscale: 'YlGnBu'
+            }
+        }];
+
+        var bubblelayout = {
+            title: 'Bacteria Cultures Per Sample',
+        };
+
+        Plotly.newPlot("bubble", bubblechart, bubblelayout);
+    });
+};
+// Set function to change chart info when new id selected
+function optionChanged(SelectedID) {
+    Charts(SelectedID);
+    // MetaData(SelectedID);
+};
